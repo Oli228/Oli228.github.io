47e318eb87d8b7449213b067adaf7547c020b468
Oli228 <123141993+Oli228@users.noreply.github.com>
2023-01-28 18:53:29 +0900

    CSS rollback

--- js/scripts.js
+++ js/scripts.js
@@ -136,6 +136,35 @@ $(document).ready(function () {
 
     });
 
+    /********************** Social Share buttons ***********************/
+    var share_bar = document.getElementsByClassName('share-bar');
+    var po = document.createElement('script');
+    po.type = 'text/javascript';
+    po.async = true;
+    po.src = 'https://apis.google.com/js/platform.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(po, s);
+
+    for (var i = 0; i < share_bar.length; i++) {
+        var html = '<iframe allowtransparency="true" frameborder="0" scrolling="no"' +
+            'src="https://platform.twitter.com/widgets/tweet_button.html?url=' + encodeURIComponent(window.location) + '&amp;text=' + encodeURIComponent(document.title) + '&amp;via=ramswarooppatra&amp;hashtags=ramandantara&amp;count=horizontal"' +
+            'style="width:105px; height:21px;">' +
+            '</iframe>' +
+
+            '<iframe src="//www.facebook.com/plugins/like.php?href=' + encodeURIComponent(window.location) + '&amp;width&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;share=true&amp;height=21&amp;appId=101094500229731&amp;width=150" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:150px; height:21px;" allowTransparency="true"></iframe>' +
+
+            '<div class="g-plusone" data-size="medium"></div>';
+
+        // '<iframe src="https://plusone.google.com/_/+1/fastbutton?bsv&amp;size=medium&amp;url=' + encodeURIComponent(window.location) + '" allowtransparency="true" frameborder="0" scrolling="no" title="+1" style="width:105px; height:21px;"></iframe>';
+
+        share_bar[i].innerHTML = html;
+        share_bar[i].style.display = 'inline-block';
+    }
+
+    /********************** Embed youtube video *********************/
+    $('.player').YTPlayer();
+
+
     /********************** Toggle Map Content **********************/
     $('#btn-show-map').click(function () {
         $('#map-content').toggleClass('toggle-map-content');
@@ -155,23 +184,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "김용준 & 진소현 결혼식",
+            title: "Ram and Antara's Wedding",
 
             // Event start date
-            start: new Date('May 20, 2023 11:00'),
+            start: new Date('Nov 27, 2017 10:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('May 20, 2023 12:00'),
+            end: new Date('Nov 29, 2017 00:00'),
 
             // Event Address
-            address: 'Wonju, Repblic of Korea',
+            address: 'ITC Fortune Park Hotel, Kolkata',
 
             // Event Description
-            description: "참석해주셔서 감사합니다."
+            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
         }
     });
 
@@ -185,11 +214,11 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== ''
-            && MD5($('#invite_code').val()) !== '') {
+        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
+            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('', data)
+            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
@@ -212,7 +241,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 14243806.084788296, lng: 4495047.793813536};
+    var location = {lat: 22.5932759, lng: 88.27027720000001};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: location,
@@ -226,7 +255,7 @@ function initMap() {
 }
 
 function initBBSRMap() {
-    var la_fiesta = {lat: 14243806.084788296, lng: 4495047.793813536};
+    var la_fiesta = {lat: 20.305826, lng: 85.85480189999998};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: la_fiesta,
